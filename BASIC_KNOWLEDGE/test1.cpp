ef533d0ef8e53ade95ec4a677e0801d92cea5ae5
lulu0819 <605851326@qq.com>
2016-09-15 14:01:20 +0800

    LL - C++ VIRTUAL

---
+++ BASIC_KNOWLEDGE/test1.cpp
@@ -0,0 +1,43 @@
+﻿/*
+csdn: http://blog.csdn.net/github_30830155/article/details/52549574
+*/
+
+#include<stdio.h>
+class A
+{
+public:
+	virtual void Test()
+	{
+		printf("A test\n");
+	}
+};
+
+class B: public A
+{
+public:
+	void func()
+	{
+		Test();
+	}
+	virtual void Test()
+	{
+		printf("B test\n");
+	}
+};
+
+class C: public B
+{
+public:
+	virtual void Test()
+	{
+		printf("C test\n");
+	}
+};
+
+int main()
+{
+	C c;
+	((B *)(&c))->func();   // DISPLAY：C test
+	((B)c).func();    // DISPLAY：B test
+
+}
